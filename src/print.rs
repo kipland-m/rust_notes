59f9f9b1d4134355ef4b4747fd8759a47cf70f5b
Kipland Melton <kiplandmelton@gmail.com>
2022-06-29 15:55:14 -0500

    more examples

--- src/print.rs
+++ src/print.rs
@@ -2,7 +2,15 @@ pub fn run() {
     // print to console
     println!("Hello from the print.rs file");
 
+    // Basic formatting
     println!("{} is from {}", "Kip", "Kentucky");
 
-    println!("{0} is from {1} and {0} likes to {2}","Dude","USA","Code")
+    // Formatting using positional args
+    println!("{0} is from {1} and {0} likes to {2}","Dude","USA","Code");
+
+    // Named arguments
+    println!("{name} likes to play {game}",
+    name = "Kip",
+    game = "minecraft 2");
+
 }
