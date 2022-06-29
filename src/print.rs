6f095953ab59f4bfc605736a50bd3fbf70c76c79
Kipland Melton <kiplandmelton@gmail.com>
2022-06-29 15:26:38 -0500

    init commit

---
+++ src/print.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    // print to console
+    println!("Hello from the print.rs file");
+
+    println!("{} is from {}", "Kip", "Kentucky");
+
+    println!("{0} is from {1} and {0} likes to {2}","Dude","USA","Code")
+}
